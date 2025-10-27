a8fc13cfb73b061ab420c6939f7cecc0ce985b51
shrey0303 <shreykhakharia2003@gmail.com>
2025-10-27 09:15:00 +0530

    chore: add evaluation package module

---
+++ src/evaluation/__init__.py
@@ -0,0 +1,6 @@
+"""Evaluation metrics and reward analysis for recommendation models."""
+
+from src.evaluation.metrics import RecommendationMetrics
+from src.evaluation.reward_analyzer import RewardAnalyzer
+
+__all__ = ["RecommendationMetrics", "RewardAnalyzer"]
